0334a1c2a655299360e91f0748999e773df8d39b
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-28 11:10:14 +0100

    Modify Input form

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const InputBook = () => (
-  <form>
-    <input type="text" name="title" placeholder="Book Title" />
-    <input type="text" name="author" placeholder="Book Author" />
-    <button type="submit">Add Book</button>
-  </form>
-);
+const InputBook = (props) => {
+  const [inputBook, setInputBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  const onChange = (e) => {
+    setInputBook({
+      ...inputBook,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addBook(inputBook);
+    setInputBook({
+      title: '',
+      author: '',
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="title" placeholder="Book Title" value={inputBook.title} onChange={onChange} required />
+      <input type="text" name="author" placeholder="Book Author" value={inputBook.author} onChange={onChange} required />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
+
+InputBook.propTypes = {
+  addBook: PropTypes.func.isRequired,
+};
 
 export default InputBook;
