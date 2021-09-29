e8079111b257093d308b0e9825d01a422ad634a2
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-09-29 15:00:04 +0100

    Change author field to category

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -6,10 +6,10 @@ import { addBook } from '../redux/books/books';
 const InputBook = () => {
   const dispatch = useDispatch();
   const [title, setInputTitle] = useState('');
-  const [category, setInputAuthor] = useState('');
+  const [category, setInputCategory] = useState('');
 
   const onTitleChange = (e) => setInputTitle((e.target.value));
-  const onAuthorChange = (e) => setInputAuthor((e.target.value));
+  const onCategoryChange = (e) => setInputCategory((e.target.value));
 
   const handleSubmit = (e) => {
     const book = {
@@ -20,13 +20,13 @@ const InputBook = () => {
     dispatch(addBook(book));
     e.preventDefault();
     setInputTitle('');
-    setInputAuthor('');
+    setInputCategory('');
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="title" placeholder="Book Title" value={title} onChange={onTitleChange} required />
-      <input type="text" name="author" placeholder="Book Author" value={category} onChange={onAuthorChange} required />
+      <input type="text" name="category" placeholder="Category" value={category} onChange={onCategoryChange} required />
       <button type="submit">Add Book</button>
     </form>
   );
